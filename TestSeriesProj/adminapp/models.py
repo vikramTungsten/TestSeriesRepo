c66326875261644d31c1f8cb1afbaed9ec4e8559
vikramTungsten <vkmchandel@gmail.com>
2017-08-03 14:26:52 +0530

    change

--- TestSeriesProj/adminapp/models.py
+++ TestSeriesProj/adminapp/models.py
@@ -1,3 +1,133 @@
+from django.contrib.auth.models import User
 from django.db import models
+import pdb
+from datetime import datetime
 
 # Create your models here.
+
+ROW_STATUS = (
+    (True, 'ACTIVE'),
+    (False, 'INACTIVE')
+)
+
+SUB_STATUS = (
+    ('ACTIVE', 'ACTIVE'),
+    ('INACTIVE', 'INACTIVE')
+)
+
+UPLOAD_NOTICE_IMAGE = 'media/'
+
+
+class State(models.Model):
+    state = models.CharField(max_length=255)
+    record_status = models.BooleanField(choices=ROW_STATUS, default=True)
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField(default=datetime.now)
+    created_by = models.CharField(max_length=255, null=True)
+    updated_by = models.CharField(max_length=255, null=True)
+
+    def __unicode__(self):
+        return self.state
+
+
+class City(models.Model):
+    city = models.CharField(max_length=255)
+    state = models.ForeignKey(State, related_name='Category')
+    record_status = models.BooleanField(choices=ROW_STATUS, default=True)
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField(default=datetime.now)
+    created_by = models.CharField(max_length=255, null=True)
+    updated_by = models.CharField(max_length=255, null=True)
+
+    def __unicode__(self):
+        return self.city
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=255)
+    banner_img = models.CharField(max_length=255)
+    record_status = models.BooleanField(choices=ROW_STATUS, default=True)
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField(default=datetime.now)
+    created_by = models.CharField(max_length=255, null=True)
+    updated_by = models.CharField(max_length=255, null=True)
+
+    def __unicode__(self):
+        return self.category
+
+
+class SubCategory(models.Model):
+    subCategory = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, related_name='Category')
+    record_status = models.BooleanField(choices=ROW_STATUS, default=True)
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField(default=datetime.now)
+    created_by = models.CharField(max_length=255, null=True)
+    updated_by = models.CharField(max_length=255, null=True)
+
+    def __unicode__(self):
+        return self.subCategory
+
+
+class Section(models.Model):
+    section = models.CharField(max_length=255)
+    subCategory = models.ForeignKey(SubCategory, related_name='SubCategory')
+    record_status = models.BooleanField(choices=ROW_STATUS, default=True)
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField(default=datetime.now)
+    created_by = models.CharField(max_length=255, null=True)
+    updated_by = models.CharField(max_length=255, null=True)
+
+    def __unicode__(self):
+        return self.section
+
+
+class UserRole(models.Model):
+    role = models.CharField(max_length=50)
+    record_status = models.BooleanField(choices=ROW_STATUS, default=True)
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField(default=datetime.now)
+    created_by = models.CharField(max_length=255, null=True)
+    updated_by = models.CharField(max_length=255, null=True)
+
+    def __unicode__(self):
+        return self.role
+
+
+class UserProfile(User):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    contanct_no = models.CharField(max_length=15)
+    email = models.CharField(max_length=255)
+    user_type = models.ForeignKey(UserRole, related_name='user_role')
+    user_status = models.CharField(max_length=255, choices=SUB_STATUS, default='Active')
+    record_status = models.BooleanField(choices=ROW_STATUS, default=True)
+    created_date = models.DateTimeField(default=datetime.now)
+    updated_date = models.DateTimeField(default=datetime.now)
+    created_by = models.CharField(max_length=255, null=True)
+    updated_by = models.CharField(max_length=255, null=True)
+
+    def __unicode__(self):
+        return self.emailId
+
+
+class Teacher(User):
+    record_status = models.BooleanField(choices=ROW_STATUS, default=True)
+    created_date = models.DateTimeField(default=datetime.now)
+    updated_date = models.DateTimeField(default=datetime.now)
+    created_by = models.CharField(max_length=255, null=True)
+    updated_by = models.CharField(max_length=255, null=True)
+
+    def __unicode__(self):
+        return self.emailId
+
+
+class Student(User):
+    record_status = models.BooleanField(choices=ROW_STATUS, default=True)
+    created_date = models.DateTimeField(default=datetime.now)
+    updated_date = models.DateTimeField(default=datetime.now)
+    created_by = models.CharField(max_length=255, null=True)
+    updated_by = models.CharField(max_length=255, null=True)
+
+    def __unicode__(self):
+        return self.emailId
--- TestSeriesProj/examapp/models.py
+++ TestSeriesProj/examapp/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from adminapp.models import Section,Teacher
+
 
 # Create your models here.
+
+class Exam(models.Model):
+    name = models.CharField(max_length=255,blank=False,null=False)
+    section = models.ForeignKey(Section, related_name='section',blank=False,null=False)
+    teacher = models.ForeignKey(Teacher, related_name='teacher',blank=False,null=False)
+    total_time = models.CharField(max_length=255)
+    total_quest = models.CharField(max_length=255)
+    total_marks = models.CharField(max_length=255)
+    passing_marks = models.CharField(max_length=255)
+    sale_start_date = models.CharField(max_length=255)
+    sale_end_date = models.CharField(max_length=255)
+    exam_start_date = models.CharField(max_length=255)
+    exam_end_date = models.CharField(max_length=255)
+    result_date = models.CharField(max_length=255)
+    sale_price = models.CharField(max_length=255)
+    offer_price = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    short_desc = models.CharField(max_length=255)
+    note = models.CharField(max_length=255)
+    instruction_set = models.CharField(max_length=255)
+    offer_available = models.BooleanField()
+    record_status = models.BooleanField(choices=ROW_STATUS, default=True)
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField(default=datetime.now)
+    created_by = models.CharField(max_length=255, null=True)
+    updated_by = models.CharField(max_length=255, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+class Question(models.Model):
+    questions = models.TextField()
+    ref_image = models.CharField(max_length=255)
+    exam = models.ForeignKey(Exam, related_name='section',blank=False,null=False)
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField(default=datetime.now)
+    created_by = models.CharField(max_length=255, null=True)
+    updated_by = models.CharField(max_length=255, null=True)
+
+    def __unicode__(self):
+        return self.questions
